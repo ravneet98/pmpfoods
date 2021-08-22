0137b0bd06c4e752cf08ea5a96743be18ab16c88
ravneet98 <ravneet0298@gmail.com>
2021-08-22 14:23:42 +0530

    Starting the frontend

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              ></Rating>
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: ${product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className='btn-black'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
