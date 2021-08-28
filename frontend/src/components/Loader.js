d630eddebea6558609f24e59895b5ff89684b414
ravneet98 <ravneet0298@gmail.com>
2021-08-28 11:57:10 +0530

    Admin Screens - Part 1

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Spinner } from "react-bootstrap";
+import { css } from "@emotion/react";
+import HashLoader from "react-spinners/HashLoader";
+
+const override = css`
+  display: block;
+  margin: auto;
+  border-color: red;
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+`;
 
 const Loader = () => {
-  return (
-    <Spinner
-      animation='border'
-      role='status'
-      style={{
-        width: '100px',
-        height: '100px',
-        margin: 'auto',
-        display: 'block',
-      }}
-    >
-      <span className='sr-only'>Loading...</span>
-    </Spinner>
-  )
-}
+  let [color, setColor] = useState("#455A64");
+  return <HashLoader css={override} color={color} size={50} />;
+};
 
-export default Loader
+export default Loader;
