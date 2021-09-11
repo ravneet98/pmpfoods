a096820ce7f3bd4dcf147741607d4b764b8486d5
ravneet98 <ravneet0298@gmail.com>
2021-09-11 20:17:13 +0530

    Product.js Image Fix 2

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -24,7 +24,7 @@ const Product = ({ product }) => {
           src={product.image}
           variant='top'
           style={{ width: '100%',
-    height: '15vw',
+    height: '20rem',
     objectFit: 'cover'}}
           
         />
